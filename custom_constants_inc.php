7765dff9e8d6f0b3d1c58a7fff50d4568da1467f
Anton Arsenij <A.Arsenij@gmx.de>
2015-08-26 14:02:01 +0200

    Deleted unused lines

--- custom_constants_inc.php
+++ custom_constants_inc.php
@@ -14,24 +14,9 @@
         define ( 'FEEDBACK', 20 );
 	define ( 'POSTPONED', 25 );
 	define ( 'FUNDING_NEEDED', 35 );
-//	define ( 'FUNDING NEEDED', 35 );
         define ( 'ASSIGNED', 50 );
 	define ( 'FIXING_ACC_TO_PRIO', 60);
-//	define ( 'FIXING ACC. TO PRIO', 60);
         define ( 'RESOLVED', 80 );
         define ( 'CLOSED', 90);
-
-/*
-	# Custom status code
-	define ( 'OPEN', 10);
-	define ( 'UNASSIGNED', 15 );
-//      define ( 'feedback', 20 );
-	define ( 'POSTPONED', 25 );
-	define ( 'FUNDING NEEDED', 35 );
-//      define ( 'assigned', 50 );
-	define ( 'FIXING ACC. TO PRIO', 60);
-//      define ( 'resolved', 80 );
-//      define ( 'closed', 90);
-*/
 
 ?>
