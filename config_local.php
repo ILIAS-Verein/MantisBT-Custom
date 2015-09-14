b23a9ea271659f6f306e74a7bc48a769b285b6fd
Anton Arsenij <A.Arsenij@gmx.de>
2015-09-14 12:04:47 +0200

    Colours modified

--- config_local.php
+++ config_local.php
@@ -14,13 +14,13 @@
 	#Revised enum string with new custom statuses
     	$g_status_enum_string =  '10:open,15:unassigned,20:feedback,25:postponed,35:funding needed,50:assigned,60:fixing acc to prio,80:resolved,90:closed';
 
-
-    	# Status color additions
-        $g_status_colors['open'] = '#fcbdbd'; # red    (scarlet red #ef2929)
-    	$g_status_colors['unassigned']              = '#cd853f'; // brown (peru  #cd853f)
-    	$g_status_colors['postponed']               = '#ffcd85'; // orange (orango  #f57900)
-    	$g_status_colors['funding needed']          = '#ffd700'; // yellow (gold  #ffd700)
-    	$g_status_colors['fixing acc to prio']      = '#c2dfff'; // blue (sky blue    #729fcf)
+	# Status color additions
+        $g_status_colors['open'] 		= '#fcbdbd'; // red    (scarlet red #ef2929)
+        $g_status_colors['unassigned']          = '#D2B48C'; // brown (tan  #D2B48C)
+        $g_status_colors['postponed']           = '#ffcd85'; // orange (orango  #f57900)
+        $g_status_colors['funding needed']      = '#FFFACD'; // yellow (LemonChiffon #FFFACD)
+        $g_status_colors['fixing acc to prio']  = '#c2dfff'; // blue (sky blue    #729fcf)
+        $g_status_colors['assigned']            = '#B0C4DE'; // blue (LightSteelBlue #B0C4DE)
 
 
         $g_status_enum_workflow[OPEN]   ='15:unassigned,20:feedback,25:postponed,35:funding needed,60:fixing acc to prio,80:resolved,90:closed';
